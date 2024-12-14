b8003cebc6f27c83d5c879717ac35ebd481b9af5
Максим Охонько <catloverdotpy@yandex.ru>
2024-12-14 21:37:59 +0300

    refactor. Fixtures update, admin.py - Added physics test to fixtures, - Added math test to fixtures, - Updated admin.py

--- egame/preparation/admin.py
+++ egame/preparation/admin.py
@@ -7,6 +7,12 @@ import preparation.models
 class TaskInline(django.contrib.admin.TabularInline):
     model = preparation.models.Task
     extra = 1
+    fields = (
+        preparation.models.Task.order.field.name,
+        preparation.models.Task.question.field.name,
+        preparation.models.Task.correct_answer.field.name,
+        preparation.models.Task.options.field.name,
+    )
 
 
 @django.contrib.admin.register(preparation.models.Test)
@@ -16,14 +22,14 @@ class TestAdmin(django.contrib.admin.ModelAdmin):
 
     list_display = (
         preparation.models.Test.title.field.name,
-        preparation.models.Test.exam.field.name,
+        exam_field,
     )
 
     search_fields = (
         preparation.models.Test.title.field.name,
         f"{exam_field}__{name_field}",
     )
-    list_filter = (preparation.models.Test.exam.field.name,)
+    list_filter = (exam_field,)
     inlines = [TaskInline]
 
 
@@ -33,19 +39,27 @@ class TaskAdmin(django.contrib.admin.ModelAdmin):
     exam_field = preparation.models.Test.exam.field.name
     name_field = practice.models.Exam.name.field.name
     title_field = preparation.models.Test.title.field.name
+    order_field = preparation.models.Task.order.field.name
+    question_field = preparation.models.Task.question.field.name
 
     list_display = (
         "get_exam",
-        preparation.models.Task.test.field.name,
-        preparation.models.Task.question.field.name,
+        test_field,
+        order_field,
+        question_field,
         preparation.models.Task.correct_answer.field.name,
     )
     search_fields = (
-        preparation.models.Task.question.field.name,
+        question_field,
         f"{test_field}__{title_field}",
         f"{test_field}__{exam_field}__{name_field}",
-    )  # Поля для поиска
+    )
     list_filter = (f"{test_field}__{exam_field}",)
+
+    ordering = (
+        test_field,
+        order_field,
+    )
 
     def get_exam(self, obj):
         return obj.test.exam
